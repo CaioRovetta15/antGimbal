34248a9dbcb0874fc60e25b8604f6dcd927359a7
Caio <caiorovetta123@usp.br>
2022-11-30 16:46:57 -0300

    minor fixes

--- src/kinematics.py
+++ src/kinematics.py
@@ -7,17 +7,17 @@ import math
 import matplotlib.pyplot as plt
 import open3d as o3d
 
-def DHRobot(d1 = 0.18061, d2 = 0.00665, a1= -0.00654 , a2=0.0, alpha1=math.pi/2, alpha2=0,offset1=-math.pi/2,offset2=0):
+def DHRobot(d1 = 0.18061, d2 = 0.00665, a1= -0.00654 , a2=0.0, alpha1=math.pi/2, alpha2=-math.pi/2,offset1=-math.pi/2,offset2=math.pi/2):
     Link1 = rtb.RevoluteDH(alpha=alpha1, a=a1, d=d1,offset=offset1,qlim=[-math.pi/2,math.pi/2])
     Link2 = rtb.RevoluteDH(alpha=alpha2, a=a2, d=d2,offset=offset2,qlim=[-math.pi/2,math.pi/2])
-    Link3 = rtb.RevoluteDH(a=0.05,qlim=[0,0])
+    Link3 = rtb.PrismaticDH(a=0.0,alpha=0,theta=0,qlim=[0,6])
     name="gimbal_robot"
 
-    base=sm.SE3(0.0, 0.0, 0.0)*sm.SE3.Rx(0)*sm.SE3.Ry(0)*sm.SE3.Rz(0)
+    base=sm.SE3(0.0, 0.0, 0.0)*sm.SE3.Rx(0)*sm.SE3.Ry(0)*sm.SE3.Rz(math.pi/2)
 
     robot = rtb.SerialLink([Link1, Link2,Link3], name=name, base=base)
     #add joints limits
-
+    # robot.teach()
     robot.q=[0.0,0.0,0.0]
     return robot
 
@@ -27,7 +27,9 @@ def robotNormals(robot,robot_base,target_point=[0.0,0.0,0.0]):
     work_space = []
     for i in np.linspace(-math.pi/2.5, math.pi/2.5, 50):
             for j in np.linspace(-math.pi/2.5, math.pi/2.5, 50):
-                work_space.append(robot.fkine([i, j,0]).t)
+                # for k in np.linspace(1, 4, 5):
+                    # work_space.append(robot.fkine([i,j,k]).t)
+                work_space.append(robot.fkine([i, j,1]).t)
 
     work_space = np.array(work_space)
     # print(work_space)
@@ -35,10 +37,10 @@ def robotNormals(robot,robot_base,target_point=[0.0,0.0,0.0]):
     pcd.points = o3d.utility.Vector3dVector(work_space)
     pcd.estimate_normals()
     pcd.orient_normals_towards_camera_location(robot_base.t)
-    o3d.visualization.draw_geometries([pcd],point_show_normal=True)
+    # o3d.visualization.draw_geometries([pcd],point_show_normal=True)
     normal_vectors=np.asarray(pcd.normals)
     
-    #calculate the distance between the line and the point
+
     dist=np.zeros((len(work_space),1))
     for i in range(len(work_space)):
         dist[i]=np.linalg.norm(np.cross(work_space[i]-np.array(target_point),normal_vectors[i]))/np.linalg.norm(normal_vectors[i])
@@ -47,17 +49,17 @@ def robotNormals(robot,robot_base,target_point=[0.0,0.0,0.0]):
     return np.array(work_space[np.argmin(dist),:]), normal_vectors[np.argmin(dist),:]
 
 
-def inverseKinematics(robot,work_space_target):
-    T = sm.SE3(work_space_target[0],work_space_target[1],work_space_target[2])
-    T.print()
-    Ttrans=sm.SE3(0,0,0)
+def inverseKinematics(robot, target_point):
+    # T = sm.SE3(work_space_target[0],work_space_target[1],work_space_target[2])
+    Ttrans=sm.SE3(target_point[0],target_point[1],target_point[2])
 
     #na simulacao o metodo utilizando as trasnsfromacoes de rotacao de pitch e yaw nao funcionou de maneira satisfatoria
-    sol_trasnf = robot.ikine_LM(Ttrans,mask=[0,0,0,1,1,1],search=False)
-    print(robot.base)
-    sol_norm = robot.ikine_LM(T,mask=[1,1,1,0,0,0],search=False)
-
-    return sol_norm[0]
+    sol_trasnf = robot.ik_lm_chan(Ttrans,we=[1,1,1,0,0,0])
+    # print(robot.base)
+    print(sol_trasnf)
+    #test on forward kinematics
+    # robot.plot(Q=sol_trasnf[0],block=True)
+    return sol_trasnf[0]
 
 
 # TODO: FIX VISUALIZATION
@@ -83,10 +85,11 @@ def inverseKinematics(robot,work_space_target):
 #     plt.show()
 
 
-def inverse_kinematics(robot=DHRobot(),robot_base=sm.SE3(1.0, 0.0, .5)):
-    work_space_target, normal_vectors = robotNormals(robot, robot_base)
-    print(work_space_target)
-    q=inverseKinematics(robot,work_space_target)
+def inverse_kinematics(robot=DHRobot(),robot_base=sm.SE3(-.6, -1.0, 0.5)):
+    target_point=[1.0,0.0,0.0]
+    # work_space_target, normal_vectors = robotNormals(robot, robot_base)
+    # print(work_space_target)
+    q=inverseKinematics(robot,target_point)
     print('inverse kinematics:',q)
     return (q[0],q[1])
     # visualize(robot,work_space,normal_vectors,[0.0,0.0,0.0])
